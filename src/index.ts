bd11750e32d792d808185a815c228e7cf0402a2b
Joachim Janke <jochnickel@yt-party.com>
2019-11-05 22:07:27 +0100

    asd

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import gtYtId from "get-youtube-id";
-const getYouTubeID = (url: string, opts?: { fuzzy: boolean }): string => gtYtId(url, opts) || getYouTubeID.ErrorID;
-getYouTubeID.ErrorID = "L-q9TQ8rPDs";
+const getYouTubeID = (url: string, opts?: { fuzzy: boolean }): string => gtYtId(url, opts) || getYouTubeID.ERROR_ID;
+getYouTubeID.ERROR_ID = "L-q9TQ8rPDs";
 export default getYouTubeID;
